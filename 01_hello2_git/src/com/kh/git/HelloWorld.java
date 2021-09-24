38dbec467b8585215a11d265ed238e3e9bcd0df8
JPLEE1991 <whitesunny65@naver.com>
2021-09-25 00:15:45 +0900

    branch1 인사말 추가

--- 01_hello2_git/src/com/kh/git/HelloWorld.java
+++ 01_hello2_git/src/com/kh/git/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World~");
 		System.out.println("Ok, bye~");
+		
+		System.out.println("안녕하세요, 저는 branch1입니다.");
 	}
 
 }
