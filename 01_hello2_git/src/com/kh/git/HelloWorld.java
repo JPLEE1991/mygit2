6ebe5eb84ac351e44bc95d9ede5fb14dd03c8938
JPLEE1991 <whitesunny65@naver.com>
2021-09-25 00:21:12 +0900

    master branch 소개내용

--- 01_hello2_git/src/com/kh/git/HelloWorld.java
+++ 01_hello2_git/src/com/kh/git/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World~");
 		System.out.println("Ok, bye~");
+		System.out.println("난 master branch에서 추가한 내용이야");
 	}
 
 }
